41a3f299893cc2121329e6de686e512729a18619
Fahid Nasir <fahid.nasir29@hotmail.com>
2015-11-01 16:51:21 +0500

    Lecture7 Initial With Wrong Entry Window

---
+++ Lecture7(ADO.NET)/ViewModels/SignupViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture7.Models
+{
+    public class SignupViewModel
+    {
+        [Required(ErrorMessage = "Email is Required"), EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is Required"), DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is Required"), DataType(DataType.Password), Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
