af15867592ff24e19a3c5d6ed2d07f490e8eee95
Fahid Nasir <fahid.nasir29@hotmail.com>
2015-11-11 22:55:33 +0500

    MultiBinding with IMultiValueConverter

---
+++ Lecture10/FullNameValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Lecture10
+{
+    public class FullNameValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string fullName = string.Empty;
+            foreach (var item in values)
+            {
+                fullName += item + " ";
+            }
+            return fullName;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
