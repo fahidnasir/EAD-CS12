f2ea28bce09dc6e356e530b7b588f15f5beea7ab
fahidnasir <fahid.nasir29@hotmail.com>
2016-01-11 21:13:54 +0500

    check username exist with ajax

---
+++ AjaxUserNameAvailabilityExample/UserNameAvailabilityExample/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UserNameAvailabilityExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult CheckUserName()
+        {
+
+            string userName = Request["UserName"];
+           
+            //check from database
+
+           // List<Student> list = new List<Student>();
+
+
+            return this.Json(true,JsonRequestBehavior.AllowGet);
+           
+        }
+    }
+}
