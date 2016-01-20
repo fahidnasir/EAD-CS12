d3f612d62d25085955fd89424829e7b425d2c342
fahidnasir <fahid.nasir29@hotmail.com>
2016-01-20 23:13:00 +0500

    basic calculator service using WCF added

---
+++ WCF/WCFCalculator/WCFCalculator/BasicCalculator.svc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFCalculator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BasicCalculator" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select BasicCalculator.svc or BasicCalculator.svc.cs at the Solution Explorer and start debugging.
+    public class BasicCalculator : IBasicCalculator
+    {
+        public double Add(double first, double second)
+        {
+            return first + second;
+        }
+
+        public double Subtract(double first, double second)
+        {
+            return first - second;
+        }
+
+        public double Multiply(double first, double second)
+        {
+            return first * second;
+        }
+
+        public double Divide(double first, double second)
+        {
+            return first / second;
+        }
+    }
+}
---
+++ WCF/WCFCalculator/WCFCalculator/IBasicCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFCalculator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IBasicCalculator" in both code and config file together.
+    [ServiceContract]
+    public interface IBasicCalculator
+    {
+        [OperationContract]
+        double Add(double first, double second);
+        double Subtract(double first, double second);
+        double Multiply(double first, double second);
+        double Divide(double first, double second);
+    }
+}
